0b084ebce2398aeae353345af66c2cb470e9045e
Maciek Tomczynski <mj.tomczynski@gmail.com>
2019-01-17 10:20:27 +0100

    Gradle tweaks and version bump

--- app/src/main/java/com/maciejko/share/App.kt
+++ app/src/main/java/com/maciejko/share/App.kt
@@ -1,13 +1,15 @@
 package com.maciejko.share
 
 import android.app.Application
+import com.maciejko.share.di.appModule
 import org.koin.android.ext.android.startKoin
 
+@Suppress("unused")
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
 
-        startKoin(this, listOf())
+        startKoin(this, listOf(appModule))
     }
 }
