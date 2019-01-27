450f10becf670676b9fdd4db5dcd385bd9c71d77
Maciek Tomczynski <mj.tomczynski@gmail.com>
2019-01-27 19:04:05 +0100

    Added basic decoration ot coordinated group list

--- app/src/main/java/com/maciejko/share/feature/master/coordinated_group_list/CoordinatedGroupListFragment.kt
+++ app/src/main/java/com/maciejko/share/feature/master/coordinated_group_list/CoordinatedGroupListFragment.kt
@@ -7,9 +7,11 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.DividerItemDecoration
 import com.maciejko.share.R
 import com.maciejko.share.databinding.FragmentCoordinatedGroupListBinding
 import org.koin.android.viewmodel.ext.android.viewModel
+
 
 class CoordinatedGroupListFragment : Fragment() {
 
@@ -29,6 +31,12 @@ class CoordinatedGroupListFragment : Fragment() {
                 coordinatedGroupAdapter.submitList(coordinatedGroupList)
             }
         })
+
+        val dividerItemDecoration = DividerItemDecoration(
+            requireContext(),
+            DividerItemDecoration.HORIZONTAL
+        )
+        binding.coordinatedGroupListRv.addItemDecoration(dividerItemDecoration)
 
         return binding.root
     }
