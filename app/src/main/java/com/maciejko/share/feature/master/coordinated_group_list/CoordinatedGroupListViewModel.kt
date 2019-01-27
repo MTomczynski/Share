aafd5b293e8f5adaa45db77bc9741cc9a690ac8b
Maciek Tomczynski <mj.tomczynski@gmail.com>
2019-01-27 16:27:57 +0100

    Added coordinated group items fetch from repository in list screen

---
+++ app/src/main/java/com/maciejko/share/feature/master/coordinated_group_list/CoordinatedGroupListViewModel.kt
@@ -0,0 +1,52 @@
+package com.maciejko.share.feature.master.coordinated_group_list
+
+import androidx.databinding.ObservableBoolean
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.maciejko.share.base.BaseViewModel
+import com.maciejko.share.data.CoordinatedGroup
+import com.maciejko.share.data.source.CoordinatedGroupRepository
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.schedulers.Schedulers
+import timber.log.Timber
+
+class CoordinatedGroupListViewModel(
+    private val coordinatedGroupRepository: CoordinatedGroupRepository
+) : BaseViewModel() {
+
+    private val _coordinatedGroupList = MutableLiveData<List<CoordinatedGroup>>()
+    val coordinatedGroupList: LiveData<List<CoordinatedGroup>>
+        get() = _coordinatedGroupList
+
+    val isError = ObservableBoolean(false)
+    val isLoading = ObservableBoolean(false)
+
+
+    init {
+        loadList()
+    }
+
+    fun reloadList() = loadList()
+
+    private fun loadList() {
+        coordinatedGroupRepository
+            .getCoordinatedGroups()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnSubscribe {
+                isLoading.set(true)
+                isError.set(false)
+            }
+            .doOnEvent { _, _ -> isLoading.set(false) }
+            .subscribeBy(
+                onSuccess = {
+                    _coordinatedGroupList.value = it
+                },
+                onError = {
+                    Timber.e(it)
+                    isError.set(true)
+                }
+            ).toDisposables()
+    }
+}
