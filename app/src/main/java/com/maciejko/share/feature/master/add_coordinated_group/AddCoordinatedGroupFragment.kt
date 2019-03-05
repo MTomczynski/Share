9c9781187f938308f2c31a641d83f9555f32ad7d
Maciek Tomczynski <mj.tomczynski@gmail.com>
2019-03-05 18:33:38 +0100

    Added toolbar with screen titles

--- app/src/main/java/com/maciejko/share/feature/master/add_coordinated_group/AddCoordinatedGroupFragment.kt
+++ app/src/main/java/com/maciejko/share/feature/master/add_coordinated_group/AddCoordinatedGroupFragment.kt
@@ -3,10 +3,11 @@ package com.maciejko.share.feature.master.add_coordinated_group
 import com.maciejko.share.R
 import com.maciejko.share.base.BaseFragment
 import com.maciejko.share.databinding.FragmentAddCoordinatedGroupBinding
+import com.maciejko.share.util.toAppCompatActivity
 
 class AddCoordinatedGroupFragment :
     BaseFragment<FragmentAddCoordinatedGroupBinding>(R.layout.fragment_add_coordinated_group) {
     override fun bind() {
-
+        activity?.toAppCompatActivity()?.supportActionBar?.title = getString(R.string.add_group_fragment_title)
     }
 }
---
+++ app/src/main/java/com/maciejko/share/util/ActivityExtension.kt
@@ -0,0 +1,6 @@
+package com.maciejko.share.util
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+
+fun Activity.toAppCompatActivity() = this as AppCompatActivity
